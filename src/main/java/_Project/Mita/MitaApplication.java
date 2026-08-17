51f13def6fe5afdcd1f120a9cc7449ce7680b648
s_mitani <s_mitani@cosmoroot.co.jp>
2026-08-17 13:58:38 +0900

    初期設定

---
+++ src/main/java/_Project/Mita/MitaApplication.java
@@ -0,0 +1,13 @@
+package _Project.Mita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/_Project/Mita/MitaApplicationTests.java
@@ -0,0 +1,13 @@
+package _Project.Mita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
